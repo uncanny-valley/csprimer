0278a712d0de70eddf0b82fdae405a978be7fea4
Brandon Fujii <brandon.fujii.diaz@gmail.com>
2026-09-10 23:27:23 -0400

    systems:bits and bytes:varint zig-zag encoding for signed integers

--- varint/varint.py
+++ varint/varint.py
@@ -1,38 +1,77 @@
+from abc import ABC, abstractmethod
 from typing import Final
 
 # the number of lower bits in byte that are dedicated to the payload
-PAYLOAD_LENGTH_IN_BITS: Final[int] = 7
+_PAYLOAD_LENGTH_IN_BITS: Final[int] = 7
+
 # 01111111 mask to isolate the 7-bit payload in a byte
-PAYLOAD_BIT_MASK: Final[int] = 0x7F
+_PAYLOAD_BIT_MASK: Final[int] = 0x7F
+
 # 10000000 mask to isolate the continuation bit (i.e. MSB) in a byte
-CONTINUATION_BIT_MASK: Final[int] = 0x80
+_CONTINUATION_BIT_MASK: Final[int] = 0x80
 
 
-def encode(num: int) -> bytes:
-    curr = num
-    out = bytearray()
-    while curr:
-        payload = curr & PAYLOAD_BIT_MASK
-        # discard the payload bits from the number now that we've extracted it
-        curr = curr >> PAYLOAD_LENGTH_IN_BITS
+class _Codec(ABC):
+    @abstractmethod
+    def encode(self, num: int) -> bytes: ...
 
-        # if we have more bytes to process after the current payload, set its
-        # continuation bit
-        if curr > 0:
-            payload |= CONTINUATION_BIT_MASK
-        out.append(payload)
-
-    return bytes(out)
+    @abstractmethod
+    def decode(self, data: bytes) -> int: ...
 
 
-def decode(data: bytes) -> int:
-    out = 0
-    # counter to track how many bits we'll need to shift to place the current byte
-    # into position in `out`
-    shift = 0
-    for byte in data:
-        payload = byte & PAYLOAD_BIT_MASK
-        mask = payload << shift
-        out |= mask
-        shift += PAYLOAD_LENGTH_IN_BITS
-    return out
+class UnsignedVarintCodec(_Codec):
+    """Variable-length integer encoding for unsigned integers"""
+
+    def encode(self, num: int) -> bytes:
+        if num < 0:
+            raise ValueError("`num` must be non-negative")
+
+        curr = num
+        out = bytearray()
+        while curr:
+            payload = curr & _PAYLOAD_BIT_MASK
+            # discard the payload bits from the number now that we've extracted it
+            curr >>= _PAYLOAD_LENGTH_IN_BITS
+
+            # if we have more bytes to process after the current payload, set its
+            # continuation bit
+            if curr > 0:
+                payload |= _CONTINUATION_BIT_MASK
+            out.append(payload)
+
+        return bytes(out)
+
+    def decode(self, data: bytes) -> int:
+        out = 0
+        # counter to track how many bits we'll need to shift to place the current byte
+        # into position in `out`
+        shift = 0
+        for byte in data:
+            payload = byte & _PAYLOAD_BIT_MASK
+            mask = payload << shift
+            out |= mask
+            shift += _PAYLOAD_LENGTH_IN_BITS
+        return out
+
+
+class SignedVarintCodec(_Codec):
+    """Zig-zag encoding for signed integers"""
+
+    def __init__(self) -> None:
+        self._unsigned_codec = UnsignedVarintCodec()
+
+    def encode(self, num: int) -> bytes:
+        if num < 0:
+            num = -2 * num - 1
+        else:
+            num *= 2
+
+        return self._unsigned_codec.encode(num=num)
+
+    def decode(self, data: bytes) -> int:
+        num = self._unsigned_codec.decode(data=data)
+        # if the number is odd, then we know it represents a negative number
+        if num & 1 == 1:
+            return -num // 2
+
+        return num // 2
--- varint/varint_test.py
+++ varint/varint_test.py
@@ -1,33 +1,69 @@
+import struct
 from typing import Final
 
 import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
-from varint import decode, encode
+from varint import SignedVarintCodec, UnsignedVarintCodec
 
 TESTDATA_PATH: Final[str] = "./varint/testdata"
 
 
-def read_testdata_as_bytes(filename: str) -> bytes:
+def read_file_as_int(filename: str) -> int:
     f = open(f"{TESTDATA_PATH}/{filename}", "rb")
-    return f.read()
+    return int(struct.unpack(">Q", f.read())[0])
 
 
-@pytest.mark.parametrize(
-    "filename",
-    (
-        "1.uint64",
-        "150.uint64",
-        "maxint.uint64",
-    ),
-)
-def test_uint64_file_cases(filename: str) -> None:
-    data = read_testdata_as_bytes(filename=filename)
-    num = int.from_bytes(data)
-    assert decode(data=encode(num=num)) == num
+class TestUnsignedIntegers:
+    @pytest.fixture
+    def codec(self) -> UnsignedVarintCodec:
+        return UnsignedVarintCodec()
+
+    def test_basic_unsigned_encode(self, codec: UnsignedVarintCodec) -> None:
+        assert codec.encode(num=150) == b"\x96\x01"
+
+    def test_basic_unsigned_decode(self, codec: UnsignedVarintCodec) -> None:
+        assert codec.decode(data=b"\x96\x01") == 150
+
+    def test_given_num_is_negative_when_encode_then_raise_value_error(
+        self, codec: UnsignedVarintCodec
+    ) -> None:
+        with pytest.raises(ValueError):
+            codec.encode(num=-1)
+
+    @pytest.mark.parametrize(
+        "filename",
+        (
+            "1.uint64",
+            "150.uint64",
+            "maxint.uint64",
+        ),
+    )
+    def test_uint64_file_round_trips(
+        self, codec: UnsignedVarintCodec, filename: str
+    ) -> None:
+        num = read_file_as_int(filename=filename)
+        assert codec.decode(data=codec.encode(num=num)) == num
+
+    @given(num=st.integers(min_value=0, max_value=1 << 30))
+    def test_unsigned_integer_round_trips(self, num: int) -> None:
+        codec = UnsignedVarintCodec()
+        assert codec.decode(data=codec.encode(num=num)) == num
 
 
-@given(num=st.integers(min_value=0, max_value=1 << 30))
-def test_uint64_positive_integers(num: int) -> None:
-    assert decode(data=encode(num=num)) == num
+class TestSignedIntegers:
+    @pytest.fixture
+    def codec(self) -> SignedVarintCodec:
+        return SignedVarintCodec()
+
+    def test_basic_signed_encode(self, codec: SignedVarintCodec) -> None:
+        assert codec.encode(num=-150) == b"\xab\x02"
+
+    def test_basic_signed_decode(self, codec: SignedVarintCodec) -> None:
+        assert codec.decode(data=b"\xab\x02") == -150
+
+    @given(num=st.integers(min_value=-(1 << 30), max_value=1 << 30))
+    def test_signed_integer_round_trips(self, num: int) -> None:
+        codec = SignedVarintCodec()
+        assert codec.decode(data=codec.encode(num=num)) == num
